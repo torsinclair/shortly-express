1f67eaa21f0afd44904d35a1efc4e59d288a538b
torsinclair <tor@romanesco.se>
2016-03-28 20:33:06 -0700

    Add user tablename and bcrypt with Promise

--- app/models/user.js
+++ app/models/user.js
@@ -5,6 +5,22 @@ var Promise = require('bluebird');
 
 
 var User = db.Model.extend({
+  tableName: 'users',
+  initialize: function() {
+    this.on('creating', this.hashPassword, this);
+  },
+  hashPassword: function(model, attrs, options) {
+    return new Promise(function(resolve, reject) {
+      bcrypt.hash(model.attributes.password, null, null, function(err, hash) { // hash
+        if (err) {
+          reject(err);
+        }
+        model.set('password', hash);
+        resolve(hash);
+      });
+    });
+  }
+
 });
 
 module.exports = User;
